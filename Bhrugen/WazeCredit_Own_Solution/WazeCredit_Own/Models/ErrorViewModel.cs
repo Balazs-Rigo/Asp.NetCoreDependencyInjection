732f1616fa32bf78fcdb4857b53b4c1b591452fb
DESKTOP-K7AA679\Mackosajt <rigo.balazs86@gmail.com>
2022-09-25 17:02:26 +0200

    Project Created. End of section 1

---
+++ Bhrugen/WazeCredit_Own_Solution/WazeCredit_Own/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WazeCredit_Own.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
